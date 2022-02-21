20c9b242339ca4c0792e3fe6c561baca3360eb4d
kiaraturner <kiaraturner241204@gmail.com>
2022-02-21 15:56:14 +0800

    made pill thing crouch, didnt want to cry this time but i did fuck up like half my code. got it to work tho :)

--- learning/Assets/scripts/PlayerController.cs
+++ learning/Assets/scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public float gravity = 3.5f;
     public float speed = 2f;
+    public float crouchSpeedMultiplier = 0.5f;
     public float jumpForce = 0.5f;
 
     private CharacterController controller;
     private Vector3 motion;
     private float currentSpeed = 0;
     private float velocity = 0;
+    private bool crouching = false;
+    private bool isGrounded = false;
 
     void Awake()
     {
@@ -24,7 +27,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        motion = Vector3.zero;
+        isGrounded = controller.isGrounded;
+        if (controller.isGrounded == true)
+
+            motion = Vector3.zero;
         bool grounded = controller.isGrounded;
 
         if (grounded == true)
@@ -40,14 +46,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (controller.isGrounded == true)
+        if (isGrounded == true)
         {
-            if (Input.GetKeyDown(KeyCode.Space) == true)
-                velocity = jumpForce;
+            if (crouching == false)
+
+            {
+                {
+                    if (Input.GetKeyDown(KeyCode.Space) == true)
+                    {
+                        velocity = jumpForce;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.C) == true)
+                    {
+                        crouching = true;
+                        currentSpeed = speed * crouchSpeedMultiplier;
+                        controller.height = 1;
+                    }
+
+
+                }
+
+            }
+        }
+        if (crouching == true)
+        {
+
+            if (Input.GetKeyUp(KeyCode.C) == true)
+            {
+                crouching = false;
+                currentSpeed = speed;
+                controller.height = 2;
+            }
         }
         ApplyMovement();
     }
-
     void ApplyMovement()
     {
         float inputX = Input.GetAxisRaw("Vertical") * currentSpeed;
@@ -57,4 +89,5 @@ public class PlayerController : MonoBehaviour
         motion.y += velocity * Time.deltaTime;
         controller.Move(motion);
     }
+
 }
