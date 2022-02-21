39570b26d858e36e89bbd1fe1fefccbc983027ea
kiaraturner <kiaraturner241204@gmail.com>
2022-02-21 16:20:03 +0800

    added health potion, was the least difficult thing today <3

--- learning/Assets/Health.cs
+++ learning/Assets/Health.cs
@@ -11,11 +11,22 @@ public class Health : MonoBehaviour
 
     private float currentHealth = 0;
     
+    public static Health Instance { get; private set; }
 
+    private void Awake()
+    {
+        //sety singleton value <3
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            enabled = false;
+        }
+    }
 
-
-
-// Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
 {
     currentHealth = maxHealth;
